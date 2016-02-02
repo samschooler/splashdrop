189dc100bc007327bbe7cdc8333be820e4f9a42a
Sam Schooler <sam@mycube.co>
2016-02-01 17:54:01 -0700

    add splashup

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,8 @@
 module.exports = {
   db: process.env.OPENSHIFT_MONGODB_DB_URL,
-  root: process.cwd()
+  root: process.cwd(),
+  dropbox: {
+    key: process.env.DROPBOX_KEY,
+    secret: process.env.DROPBOX_SECRET
+  }
 };
